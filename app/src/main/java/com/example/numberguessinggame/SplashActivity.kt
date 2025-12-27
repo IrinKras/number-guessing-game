8f0a9af898742ab486b55843ca02ca001438cc45
IrinKras <irinkov0606@gmail.com>
2025-12-27 16:55:03 +0200

    Initial commit - Number Guessing Game

---
+++ app/src/main/java/com/example/numberguessinggame/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.numberguessinggame
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.example.numberguessinggame.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var splashBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = splashBinding.root
+        setContentView(view)
+
+        splashBinding.imageViewSplash.animation = AnimationUtils.loadAnimation(this,R.anim.image_animation)
+        splashBinding.textViewSplash.animation = AnimationUtils.loadAnimation(this,R.anim.text_animation)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+
+    }
+}
+
